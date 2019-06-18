35b9ec5c65131e6d762e16601210248fc2bf0d2b
杨胜举 <yangshengju@jsh.com>
2019-06-19 05:09:36 +0800

    project inited

---
+++ hwork-security-demo/src/main/java/com/hwork/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.hwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.swing.*;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class,args);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello spring security!";
+    }
+}
