a7b434aa36502f50bc86a97a12795ac434002527
杨胜举 <yangshengju@jsh.com>
2020-03-25 07:15:48 +0800

    app认证服务器配置优化及资源服务器启用

--- hwork-security-app/src/main/java/com/hwork/app/AppSecurityConfig.java
+++ hwork-security-app/src/main/java/com/hwork/app/AppSecurityConfig.java
@@ -2,7 +2,10 @@ package com.hwork.app;
 
 import com.hwork.core.properties.SecurityProperties;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
@@ -24,7 +27,8 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http//.addFilterBefore(verificationCodeValidateFilter, UsernamePasswordAuthenticationFilter.class)
-                .formLogin()
+//                .formLogin()
+                .httpBasic()
 //                .loginPage("/authentication/required")
 //                .loginProcessingUrl("/authentication/form")
 //                .successHandler(customAuthenticationSuccessHandler)
@@ -33,5 +37,11 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(securityProperties.getBrowser().getNotAuthList().split(","))
                 .permitAll()
                 .anyRequest().hasRole("USER");
+    }
+
+    @Override
+    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
--- hwork-security-app/src/main/java/com/hwork/app/HworkAuthorizationServerConfig.java
+++ hwork-security-app/src/main/java/com/hwork/app/HworkAuthorizationServerConfig.java
@@ -3,9 +3,12 @@ package com.hwork.app;
 import com.hwork.core.properties.SecurityProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
@@ -14,9 +17,25 @@ public class HworkAuthorizationServerConfig extends AuthorizationServerConfigure
     @Autowired
     private SecurityProperties securityProperties;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory().withClient("hwork").secret("hworkSecret").authorizedGrantTypes("authorization_code")
+        clients.inMemory().withClient("hwork").secret(passwordEncoder.encode("hworkSecret")).authorizedGrantTypes("authorization_code","password")
                 .scopes("all").authorities("USER").redirectUris("http://localhost/user/authInfo");
+    }
+
+    /*@Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.passwordEncoder(NoOpPasswordEncoder.getInstance());
+    }*/
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager);
     }
 }
